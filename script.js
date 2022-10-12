74369f9aa075332ba9e3a512922733e12e4ec76b
ThomasLuton <luton.thomas60@gmail.com>
2022-10-12 10:54:35 +0200

    introduction du bug

--- script.js
+++ script.js
@@ -48,7 +48,7 @@ function ageControl(){
     var age = document.getElementById('age').value;
     //J'utilise parseInt car 'value' renvoie une chaine de caractère et non un nombre entier
     // car sinon age pourrait avoir la forme => 
-    // age = "chaine de caractère";
+    age = "chaine de caractère";
     var ageInt = parseInt(age);
     if(ageInt < 18){
         document.getElementById('condition').innerHTML = 'Tu es mineur !';
