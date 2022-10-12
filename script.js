e2a8dd5be232dd3bc83aefc071d1cef6e6a6e902
ThomasLuton <luton.thomas60@gmail.com>
2022-10-12 19:31:42 +0200

    version proto

--- script.js
+++ script.js
@@ -1,6 +1,76 @@
 var scoreX = 0;
 var scoreO = 0;
 
+function verify(){
+    const game = document.getElementsByClassName('case');
+
+    //verif ligne 1
+    if(game[0].innerHTML==game[1].innerHTML && game[0].innerHTML==game[2].innerHTML && game[0].innerHTML=="X"){
+        victoireX();
+    }
+    if(game[0].innerHTML==game[1].innerHTML && game[0].innerHTML==game[2].innerHTML && game[0].innerHTML=="O"){
+        victoireO();
+    }
+
+    //verif ligne 2
+    if(game[3].innerHTML==game[4].innerHTML && game[3].innerHTML==game[5].innerHTML && game[3].innerHTML=="X"){
+        victoireX;
+    }
+    if(game[3].innerHTML==game[4].innerHTML && game[3].innerHTML==game[5].innerHTML && game[3].innerHTML=="O"){
+        victoireO;
+    }
+    
+    //verif ligne 3
+    if(game[6].innerHTML==game[7].innerHTML && game[6].innerHTML==game[8].innerHTML && game[6].innerHTML=="X"){
+        victoireX();
+    }
+    if(game[6].innerHTML==game[7].innerHTML && game[6].innerHTML==game[8].innerHTML && game[6].innerHTML=="O"){
+        victoireO();
+    }
+
+    //verif colonne 1
+    if(game[0].innerHTML==game[3].innerHTML && game[0].innerHTML==game[6].innerHTML && game[0].innerHTML=="X"){
+        victoireX();
+    }
+    if(game[0].innerHTML==game[3].innerHTML && game[0].innerHTML==game[6].innerHTML && game[0].innerHTML=="O"){
+        victoireO();
+    }
+
+    //verif colonne 2
+    if(game[1].innerHTML==game[4].innerHTML && game[1].innerHTML==game[7].innerHTML && game[1].innerHTML=="X"){
+        victoireX();
+    }
+    if(game[1].innerHTML==game[4].innerHTML && game[1].innerHTML==game[7].innerHTML && game[1].innerHTML=="O"){
+        victoireO();
+    }
+
+    //verif colonne 3
+    if(game[2].innerHTML==game[5].innerHTML && game[2].innerHTML==game[8].innerHTML && game[2].innerHTML=="X"){
+        victoireX();
+    }
+    if(game[2].innerHTML==game[5].innerHTML && game[2].innerHTML==game[8].innerHTML && game[2].innerHTML=="O"){
+        victoireO();
+    }
+
+    //verif diago 1
+    if(game[0].innerHTML==game[4].innerHTML && game[0].innerHTML==game[8].innerHTML && game[0].innerHTML=="X"){
+        victoireX;
+    }
+    if(game[0].innerHTML==game[4].innerHTML && game[0].innerHTML==game[8].innerHTML && game[0].innerHTML=="O"){
+        victoireO;
+    }
+
+    //verif diago 2
+    if(game[2].innerHTML==game[4].innerHTML && game[2].innerHTML==game[6].innerHTML && game[2].innerHTML=="X"){
+        victoireX();
+    }
+    if(game[2].innerHTML==game[4].innerHTML && game[2].innerHTML==game[6].innerHTML && game[2].innerHTML=="O"){
+        victoireO();
+    }
+
+}
+
+
 function clearBoard(){
     const test = document.getElementsByClassName('case');
     for(var i = 0; i<test.length; i++){
@@ -56,3 +126,4 @@ function ageControl(){
         document.getElementById('condition').innerHTML = 'Tu es majeur !';
     }
 }
+
